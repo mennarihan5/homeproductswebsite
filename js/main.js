ab02fffe0b5bf503f3e37eaab03741290678e694
mennarihan5 <menna1986@live.com>
2023-10-06 23:36:58 +0300

    finalizing the design and functionality

---
+++ js/main.js
@@ -0,0 +1,144 @@
+// SELECT ELEMENTS
+const productsCont = document.querySelector(".products-container");
+const cartItemsLi = document.querySelector(".cart-items");
+const total = document.querySelector(".subtotal");
+const cartPopUp = document.querySelector(".cart");
+const closeBtn = document.querySelector(".close-icon");
+// const cartIcon = document.querySelector(".cart-icon");
+const cardIcon = document.getElementsByClassName("card-icon");
+
+console.log(cardIcon)
+
+
+
+// //HIDE CART
+// const hideCart = cartPopUp.style.display = "none";
+
+// //SHOW CART
+// const showCart = cartPopUp.style.display = "block";
+
+// OPEN CART
+// for (let i = 0; i < cardIcon.length; i++) {
+//     cardIcon[i].addEventListener("click", () => {
+//         cartPopUp.classList.add("open");
+        
+//     });
+//     console.log(cardIcon[i]);
+// }
+ function openCart() {
+    cartPopUp.classList.add('open');
+ }
+
+
+// //CLOSE CART
+closeBtn.addEventListener("click", () => {
+    cartPopUp.classList.add("close");
+    cartPopUp.classList.remove("open");
+
+})
+
+// RENDER PRODUCTS
+function renderProducts() {
+    products.forEach((product) => {
+
+        productsCont.innerHTML += `<div class="product-card">
+        <div class="card-img">
+            <img src="${product.imgSrc}">
+        </div>
+        <div class="products-text">
+            <div class="product-title">${product.name}</div>
+            <div class="product-price">${product.price} €</div>
+            <div class="product-icons">
+                <div onclick = "addToCart(${product.id}); openCart()"><i class="fa-solid fa-cart-arrow-down card-icon"></i></div>
+                <div class="fav-icon"><i class="fa-solid fa-heart"></i></div>
+            </div>
+        </div>
+    </div>`
+      
+    })
+}
+
+renderProducts();
+
+// CART ARRAY
+let cart = [];
+
+
+// ADD TO CART
+function addToCart(id) {
+    if (cart.some((el) => el.id === id)) {
+        changeQuantity("plus", id);
+    } else {
+    let item = products.find((el) => el.id === id);
+    cart.push({
+        ...item,
+        numberOfUnits: 1,
+    });
+
+    console.log(cart);
+    }
+
+   updateCart(); 
+}
+
+// UPDATE CART
+function updateCart() {
+    renderCartItems();
+    renderSubtotal();
+}
+
+// CALCULATE AND RENDER SUBTOTAL
+function renderSubtotal() {
+    let totalPrice = 0, totalItems = 0;
+
+    cart.forEach((el) => {
+        totalPrice += el.price * el.numberOfUnits;
+        totalItems += el.numberOfUnits;
+    })
+    total.innerHTML = `Subtotal ${totalItems} items: ${totalPrice} €`;
+  
+}
+
+function renderCartItems() {
+    // clear cart elements
+    cartItemsLi.innerHTML = ""; 
+    cart.forEach((el) => {
+        cartItemsLi.innerHTML += `<div class="cart-item">
+        <div class="item-info">
+            <div class="item-info-img"><img src="${el.imgSrc}" alt=""></div>
+            <h4 class="item-info-text">${el.name}</h4>
+        </div>
+        <div class="unit-price">
+            <p>${el.price} €</p>
+        </div>
+        <div class="units">
+            <div class="minus-btn" onclick = "changeQuantity('minus', ${el.id})">-</div>
+            <div class="number">${el.numberOfUnits}</div>
+            <div class="plus-btn" onclick = "changeQuantity('plus', ${el.id})">+</div>
+        </div>
+    </div>`
+    })
+}
+
+
+// CHANGE QUANTITY OF ITEMS
+function changeQuantity(action, id) {
+    cart = cart.map((el) => {
+
+        let numberOfUnits = el.numberOfUnits;
+
+        if (el.id === id) {
+            if (action === "minus" && numberOfUnits > 1) {
+                numberOfUnits--;
+            }else if (action === "plus" && numberOfUnits < el.instock) {
+                numberOfUnits++;
+            }
+        }
+        return {
+            ...el,
+            numberOfUnits,
+        };
+    });
+    updateCart();
+}
+
---
+++ js/products.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+      id: 0,
+      name: "Teapot Grey",
+      price: 29.99,
+      instock: 100,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "../images/ab313-a659_tea_1220x1220_brandvariant.jpg",
+    },
+    {
+      id: 1,
+      name: "Teapot Yellow",
+      price: 24.99,
+      instock: 43,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "../images/ab346-a601-ab86_coffee-s-light-yellow_1220x1220_brandvariant.jpg",
+    },
+    {
+      id: 2,
+      name: "Teapot Green",
+      price: 35.99,
+      instock: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "../images/ab346-a602-ab97_coffee-m-mint_1220x1220_brandvariant.jpg",
+    },
+    {
+      id: 3,
+      name: "French Press Yellow",
+      price: 41.99,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "../images/ab396-a650_french-press-brewer-clear_gb_1220x1220_brandvariant.jpg",
+    },
+    {
+      id: 4,
+      name: "French Press Aqua",
+      price: 41.99,
+      instock: 4,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "../images/ab396-a938_french-press-brewer-aqua_gb_1220x1220_brandvariant.jpg",
+    },
+    {
+      id: 5,
+      name: "Plate White Blue/Yellow",
+      price: 34.99,
+      instock: 40,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "../images/ab612-a430-ac62_sobremesa-plate-245-set-of-2-blue-and-yellow_gb_1220x1220_brandvariant.jpg",
+    },
+  ];
